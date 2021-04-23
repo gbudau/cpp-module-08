2040941b1c8395770447eea1765a486344e6845b
gbudau <54190400+gbudau@users.noreply.github.com>
2021-04-23 23:55:23 +0200

    ex01 Add tests and addNumbers() function template
    Added a function template to add numbers using iterators as parameters
    Added unit tests

--- ex01/span.hpp
+++ ex01/span.hpp
@@ -1,6 +1,7 @@
 #ifndef SPAN_HPP_
 #define SPAN_HPP_
 #include <vector>
+#include <string>
 
 class Span {
  public:
@@ -13,11 +14,24 @@ class Span {
 	unsigned int	shortestSpan() const;
 	unsigned int	longestSpan() const;
 
+	template <typename T>
+	void			addNumbers(T start, T end);
 
  private:
 	Span();
 	unsigned int		_max_size;
 	std::vector<int>	_data;
 };
+
+template <typename T>
+void	Span::addNumbers(T start, T end) {
+	if (start == end) {
+	throw std::logic_error(std::string(
+			"Iterators have invalid range"));
+	}
+	while (start != end) {
+		addNumber(*start++);
+	}
+}
 
 #endif  // SPAN_HPP_
