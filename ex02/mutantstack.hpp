c35cf302c12e48e7d363c64c50c8b1442df913c6
gbudau <54190400+gbudau@users.noreply.github.com>
2021-04-25 01:50:34 +0200

    ex02 Add const iterators
    Added const iterators

--- ex02/mutantstack.hpp
+++ ex02/mutantstack.hpp
@@ -12,13 +12,24 @@ class MutantStack: public std::stack<T, Container> {
 							return *this;
 	 					}
 	 ~MutantStack() {}
-	 typedef typename Container::iterator			iterator;
-	 typedef typename Container::reverse_iterator	reverse_iterator;
+	 typedef typename Container::iterator				iterator;
+	 typedef typename Container::const_iterator			const_iterator;
+	 typedef typename Container::reverse_iterator		reverse_iterator;
+	 typedef typename Container::const_reverse_iterator	const_reverse_iterator;
 
 	 iterator			begin() { return std::stack<T, Container>::c.begin(); }
-	 reverse_iterator	rbegin() { return std::stack<T, Container>::c.rbegin(); }
 	 iterator			end() { return std::stack<T, Container>::c.end(); }
+	 const_iterator		begin() const {
+								return std::stack<T, Container>::c.begin(); }
+	 const_iterator		end() const {
+									return std::stack<T, Container>::c.end(); }
+	 reverse_iterator	rbegin() {
+								return std::stack<T, Container>::c.rbegin(); }
 	 reverse_iterator	rend() { return std::stack<T, Container>::c.rend(); }
+	 const_reverse_iterator	rbegin() const {
+								return std::stack<T, Container>::c.rbegin(); }
+	 const_reverse_iterator	rend() const {
+									return std::stack<T, Container>::c.rend(); }
 };
 
 #endif  // MUTANTSTACK_HPP_
