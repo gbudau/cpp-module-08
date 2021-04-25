1c75d7d833ad92c2267c42e2028dc1a33f60641a
gbudau <54190400+gbudau@users.noreply.github.com>
2021-04-25 15:37:56 +0200

    ex02 Modified assignment operator
    Assigning the underlying container to the parameter underlying container
    Improved identation

--- ex02/mutantstack.hpp
+++ ex02/mutantstack.hpp
@@ -6,30 +6,52 @@
 template <typename T, typename Container = std::deque<T> >
 class MutantStack: public std::stack<T, Container> {
  public:
-	 MutantStack() {}
-	 MutantStack(const MutantStack & src) : std::stack<T, Container>(src) {}
-	 MutantStack &		operator=(const MutantStack &) {
-							return *this;
-	 					}
-	 ~MutantStack() {}
-	 typedef typename Container::iterator				iterator;
-	 typedef typename Container::const_iterator			const_iterator;
-	 typedef typename Container::reverse_iterator		reverse_iterator;
-	 typedef typename Container::const_reverse_iterator	const_reverse_iterator;
+	MutantStack() {}
+	MutantStack(const MutantStack & src) : std::stack<T, Container>(src) {}
+	MutantStack &		operator=(const MutantStack & src) {
+		if (this != &src) {
+			std::stack<T, Container>::c = src.std::stack<T, Container>::c;
+		}
+		return *this;
+	}
+	~MutantStack() {}
 
-	 iterator			begin() { return std::stack<T, Container>::c.begin(); }
-	 iterator			end() { return std::stack<T, Container>::c.end(); }
-	 const_iterator		begin() const {
-								return std::stack<T, Container>::c.begin(); }
-	 const_iterator		end() const {
-									return std::stack<T, Container>::c.end(); }
-	 reverse_iterator	rbegin() {
-								return std::stack<T, Container>::c.rbegin(); }
-	 reverse_iterator	rend() { return std::stack<T, Container>::c.rend(); }
-	 const_reverse_iterator	rbegin() const {
-								return std::stack<T, Container>::c.rbegin(); }
-	 const_reverse_iterator	rend() const {
-									return std::stack<T, Container>::c.rend(); }
+	typedef typename Container::iterator				iterator;
+	typedef typename Container::const_iterator			const_iterator;
+	typedef typename Container::reverse_iterator		reverse_iterator;
+	typedef typename Container::const_reverse_iterator	const_reverse_iterator;
+
+	iterator			begin() {
+		return std::stack<T, Container>::c.begin();
+	}
+
+	iterator			end() {
+	    return std::stack<T, Container>::c.end();
+	}
+
+	const_iterator		begin() const {
+		return std::stack<T, Container>::c.begin();
+	}
+
+	const_iterator		end() const {
+		return std::stack<T, Container>::c.end();
+	}
+
+	reverse_iterator	rbegin() {
+		return std::stack<T, Container>::c.rbegin();
+	}
+
+	reverse_iterator	rend() {
+		return std::stack<T, Container>::c.rend();
+	}
+
+	const_reverse_iterator	rbegin() const {
+		return std::stack<T, Container>::c.rbegin();
+	}
+
+	const_reverse_iterator	rend() const {
+		return std::stack<T, Container>::c.rend();
+	}
 };
 
 #endif  // MUTANTSTACK_HPP_
